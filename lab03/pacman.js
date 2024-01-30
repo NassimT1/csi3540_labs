bfdac432c3c9398fc41f8f809a391a73aecf2700
Nassim Tiraoui <nassim.tiraoui@gmail.com>
2024-01-30 16:11:37 -0500

    completed movement of pacman and scoring system

---
+++ lab03/pacman.js
@@ -0,0 +1,90 @@
+let game = [];
+let pacmanPosition = 0;
+let fruitPosition = 0;
+let enemyPosition = 0;
+let score = 0;
+
+
+function createGame(n) {
+    if(n < 3) {
+        console.log("Game cannot be smaller than 3")
+    } else {
+        game = new Array(n).fill('.');
+        pacmanPosition = Math.floor(Math.random() * n); // Generate random position for pacman
+        game[pacmanPosition] = 'C';
+
+        do { // Do while to add position of fruit without overlaping with pacman
+            fruitPosition = Math.floor(Math.random() * n);
+        } while (fruitPosition === pacmanPosition);
+        game[fruitPosition] = '@';
+
+        do { // Do while to add position of ghost without overlaping with pacman or fruit
+            enemyPosition = Math.floor(Math.random() * n);
+        } while (enemyPosition === pacmanPosition || enemyPosition === fruitPosition);
+        game[enemyPosition] = '^';
+
+        console.log(game); // Display the game
+    }
+}
+
+function moveLeft() {
+    if (game[pacmanPosition - 1] === '^' || (pacmanPosition === 0) && (enemyPosition === game.length - 1)) { // If pacman hits the left border or ghost, end game
+        console.log("Game Over");
+        console.log(`Score: ${score}`);
+        return;
+    } 
+    else if (pacmanPosition === 0) {
+        if (fruitPosition === game.length - 1) {
+            score += 50;
+        } else {
+            score += 10;
+        }
+        game[pacmanPosition] = '.';
+        pacmanPosition = game.length - 1;
+        game[pacmanPosition] = 'C';
+        console.log(game);
+    }
+    else { // Otherwise update pacman's position and score
+        if (game[pacmanPosition - 1] === '@') {
+            score += 50;
+        } else {
+            score += 10;
+        }
+        game[pacmanPosition] = '.';
+        pacmanPosition--;
+        game[pacmanPosition] = 'C';
+        console.log(game);
+    }
+    console.log(`Score: ${score}`);
+}
+
+function moveRight() {
+    if (game[pacmanPosition + 1] === '^' || (pacmanPosition === game.length - 1) && (enemyPosition === 0)) { // If pacman hits the right border or ghost, end game
+        console.log("Game Over");
+        console.log(`Final score: ${score}`);
+        return;
+    }
+    else if (pacmanPosition === game.length - 1) {
+        game[pacmanPosition] = '.'; // Clear the current position
+        pacmanPosition = 0; // Move pacman to the leftmost position
+        if (game[pacmanPosition] === '@') { // Check if there's a fruit at the new position
+            score += 50;
+        } else {
+            score += 10;
+        }
+        game[pacmanPosition] = 'C'; // Place pacman at the new position
+        console.log(game);
+    } else { // Otherwise update pacman's position
+        if (game[pacmanPosition + 1] === '@') {
+            score += 50;
+        } else {
+            score += 10;
+        }
+        game[pacmanPosition] = '.';
+        pacmanPosition++;
+        game[pacmanPosition] = 'C';
+        console.log(game);
+    }
+    console.log(`Score: ${score}`);
+}
+
